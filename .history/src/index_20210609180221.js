54a23042bb6f795a2d0621fad9110b55b5c93b0b
范静 <fanjing04@meituan.com>
2021-06-10 11:36:43 +0800

    initnal commit

---
+++ .history/src/index_20210609180221.js
@@ -0,0 +1,76 @@
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => (typeof child === 'object' ? child : createTextElement(child))),
+    },
+  };
+}
+
+function creatDom(fiber) {
+  const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type);
+
+  const isProperty = (key) => key !== 'children';
+
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = fiber.props[name];
+    });
+
+  fiber.props.children.forEach((child) => {
+    render(child, dom);
+  });
+
+  return dom;
+}
+
+function render(element, container) {}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork !== null && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(nextUnitOfWork) {}
+
+const Didact = {
+  createElement,
+  render,
+};
+
+/** @jsx Didact.createElement */
+const element = (
+  <div>
+    <h1>
+      <p />
+      <a />
+    </h1>
+    <h2 />
+  </div>
+);
+
+const container = document.getElementById('root');
+
+Didact.render(element, container);
